4d14024235e707ce09831563989f44c55a717863
umidjanovnaa <sheraliyevaruxshona08@gmail.com>
2023-12-07 03:06:28 -0800

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# result = 0
+# for num in numbers:
+#     result = result + num
+#
+# print(result)
+
+# a = ["Kamoliddin", "Ruxshona", "Diyor"]
+# for n in a:
+#     if n == ("Kamoliddin"):
+#         print(f"{n} Ustoz")
+#     elif n == "Ruxshona":
+#         print(f"{n} O'quvchi")
+#     elif n == "Diyor":
+#         print(f"{n}O'quvchi")
+
+# a = ["Olmaliq","Toshkent","Ohangaron"]
+# for b in a:
+#     if b == "Olmaliq":
+#         print(f"{b} Shaxar")
+#     elif b == "Toshkent":
+#         print(f"{b} Poytaxt")
+#     elif b == "Ohangaron":
+#         print(f"{b} Shaxar")
+
+
+
+# a = int(input("Bugun necha kishi bilan suhbaylashdingiz: "))
+# b = str(input("1-suhbatlashgan odamingiz kim edi: "))
+# d = str(input("2-suhbatlashgan odamingiz kim edi: "))
+# c = str(input("3-suhbatlashgan odamingiz kim edi: "))
+# items = [b, d, c]
+# for x in items:
+#     print(items)
+
+
+# a = str(input("1-ismni kiriting:"))
+# b = str(input("2-ismni kiriting:"))
+# c = str(input("3-ismni kiriting:"))
+# d = str(input("4-ismni kiriting:"))
+# e = str(input("5-ismni kiriting:"))
+# names = [a, b, d, c, e]
+# for x in names:
+#     print(f"Salom {x}!")
+
+
+# a = int(input("1-sonni kirit: "))
+# b = int(input("2-sonni kirit: "))
+# d = int(input("3-sonni kirit: "))
+# c = int(input("4-sonni kirit: "))
+# e = int(input("5-sonni kirit: "))
+# y = [a, b, d, c, e]
+# for x in y:
+#     print("Sizning javoblaringiz: ", x ** 3)
